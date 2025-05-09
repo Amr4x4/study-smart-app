9e3bf0523ebaaf27e13681333f2610c1695e6f13
Amr <amranwarx2@gmail.com>
2025-05-09 23:32:15 +0300

    designing the subject card section and all the needed

---
+++ app/src/main/java/com/example/study_smart/domine/model/Task.kt
@@ -0,0 +1,10 @@
+package com.example.study_smart.domine.model
+
+data class Task(
+    val title: String,
+    val description: String,
+    val dueDate: Long,
+    val priority: Int,
+    val relatedToSubject: String,
+    val isComplete: Boolean
+)
