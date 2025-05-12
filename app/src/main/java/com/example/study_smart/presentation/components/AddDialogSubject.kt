7ac8823e36e98405bb3fb65ab4067f7906977193
Amr <amranwarx2@gmail.com>
2025-05-12 23:24:37 +0300

    Adding  Alert Dialog to Add Subject.

---
+++ app/src/main/java/com/example/study_smart/presentation/components/AddDialogSubject.kt
@@ -0,0 +1,12 @@
+package com.example.study_smart.presentation.components
+
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AddDialogSubject(
+    modifier: Modifier = Modifier
+) {
+
+}
